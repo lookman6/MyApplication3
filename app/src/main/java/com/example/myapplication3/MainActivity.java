655dfe64fdcb0912567f49d57a304c6aec91ca33
Jean christ KONDAGA <lookmanossam.sana@etu.uae.ac.ma>
2022-10-20 11:29:07 +0100

    second commit

--- app/src/main/java/com/example/myapplication3/MainActivity.java
+++ app/src/main/java/com/example/myapplication3/MainActivity.java
@@ -3,6 +3,8 @@ package com.example.myapplication3;
 import androidx.appcompat.app.AppCompatActivity;
 
 import android.os.Bundle;
+import android.widget.ArrayAdapter;
+import android.widget.ListView;
 
 public class MainActivity extends AppCompatActivity {
 
@@ -10,5 +12,15 @@ public class MainActivity extends AppCompatActivity {
     protected void onCreate(Bundle savedInstanceState) {
         super.onCreate(savedInstanceState);
         setContentView(R.layout.activity_main);
+        ListView myList = (ListView) findViewById(R.id.lstComponent);
+        String[] components = getResources().getStringArray(R.array.components);
+        ArrayAdapter<String> adapter = new ArrayAdapter<String>
+                (this,android.R.layout.simple_list_item_1,components);
+        myList.setAdapter(adapter);
+        String[] myData = {"Lundi", "Mardi",
+                "Mercredi", "Jeudi", "Vendredi",
+                "Samedi", "Dimanche"};
+        ArrayAdapter<String> adapter2 = new ArrayAdapter<String>
+                (this,android.R.layout.simple_list_item_1,myData);
     }
 }
